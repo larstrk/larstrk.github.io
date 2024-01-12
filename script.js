29b86182810bbaf4cb71f0f62295b6f358c05f33
Lars Tuerke <127292505+larstrk@users.noreply.github.com>
2024-01-12 15:57:46 +0100

    emojis

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMenu() {
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Student", "Developer", "Climber"];
+const textArray = ["Student📚", "Developer👨‍💻", "Climber🧗‍♂️"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
