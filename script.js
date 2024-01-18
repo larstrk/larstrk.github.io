aab50dc8a6d868bec663f225c1db3059725e1912
Lars Tuerke <127292505+larstrk@users.noreply.github.com>
2024-01-18 23:47:37 +0100

    Update projects

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMenu() {
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Student📚", "Developer👨‍💻", "Climber🧗‍♂️"];
+const textArray = ["Student", "Developer", "Climber"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
